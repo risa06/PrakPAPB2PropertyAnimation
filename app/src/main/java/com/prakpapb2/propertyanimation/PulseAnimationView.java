0f878b6e3f396af0a32e334326a68881eaa7f701
risa06 <risamahardika89@gmail.com>
2021-06-10 20:26:28 +0700

    second commit

--- app/src/main/java/com/prakpapb2/propertyanimation/PulseAnimationView.java
+++ app/src/main/java/com/prakpapb2/propertyanimation/PulseAnimationView.java
@@ -1,13 +1,20 @@
 package com.prakpapb2.propertyanimation;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
 
 import androidx.annotation.Nullable;
+import androidx.interpolator.view.animation.LinearOutSlowInInterpolator;
 
 public class PulseAnimationView extends View {
 
@@ -20,6 +27,8 @@ public class PulseAnimationView extends View {
     private float mX;
     private float mY;
 
+    private AnimatorSet mPulseAnimatorSet = new AnimatorSet();
+
     public PulseAnimationView(Context context) {
         this(context, null);
     }
@@ -31,6 +40,29 @@ public class PulseAnimationView extends View {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        ObjectAnimator growAnimator = ObjectAnimator.ofFloat(this, "radius", 0, getWidth());
+        growAnimator.setDuration(ANIMATION_DURATION);
+        growAnimator.setInterpolator(new LinearInterpolator());
+
+        ObjectAnimator shrinkAnimator = ObjectAnimator.ofFloat(this, "radius", getWidth(), 0);
+        shrinkAnimator.setDuration(ANIMATION_DURATION);
+        shrinkAnimator.setInterpolator(new LinearOutSlowInInterpolator());
+        shrinkAnimator.setStartDelay(ANIMATION_DELAY);
+
+        ObjectAnimator repeatAnimator = ObjectAnimator.ofFloat(this,"radius", 0, getWidth());
+        repeatAnimator.setStartDelay(ANIMATION_DELAY);
+        repeatAnimator.setDuration(ANIMATION_DURATION);
+        repeatAnimator.setRepeatCount(1);
+        repeatAnimator.setRepeatMode(ValueAnimator.REVERSE);
+
+        mPulseAnimatorSet.play(growAnimator).before(shrinkAnimator);
+        mPulseAnimatorSet.play(repeatAnimator).after(shrinkAnimator);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawCircle(mX, mY, mRadius, mPaint);
     }
 
     public void setRadius(float radius) {
@@ -42,7 +74,7 @@ public class PulseAnimationView extends View {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
-            
+
         }
         return super.onTouchEvent(event);
     }
